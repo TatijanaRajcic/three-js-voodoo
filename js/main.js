abb05420da3b9adb2575485634592c14551dea47
Tatijana Rajcic <tatijana.rajcic@essec.edu>
2020-06-30 15:03:26 +0200

    added basic three.js structure

--- js/main.js
+++ js/main.js
@@ -5,82 +5,70 @@ let scene;
 let camera;
 let renderer;
 let material;
+let geometry;
+let mesh;
 
 function init() {
   container = document.querySelector("#scene-container");
-  createScene();
-  createCamera();
-  createRenderer();
-  createMaterials();
-  createGeometries();
-
-  // renderer.setAnimationLoop(() => {
-  //   render();
-  // });
-
-  render();
+  createScene(); // universe in which my 3D objects live
+  createCamera(); // camera that enables me to see the scene
+  createMaterials(); // Materials define the surface properties of objects - that is, which material that the object looks like it is made from
+  createGeometries(); // the geometry of an object defines its shape
+  createMesh();
+  createLight();
+  createRenderer(); // this is a machine that takes a Camera and a Scene as input and outputs beautiful drawings (or renderings) onto your <canvas>.
 }
 
 function createScene() {
   scene = new THREE.Scene();
+  //scene.background = new THREE.Color("skyblue"); // the color of the scene (think about it as the walls)
 }
 
 function createCamera() {
-  camera = new THREE.PerspectiveCamera(
-    45,
-    window.innerWidth / window.innerHeight,
-    1,
-    500
-  );
-  camera.position.set(0, 0, 100);
+  const fov = 35; // field of view - the .fov parameter defines how much bigger the far clipping plane will be than the near clipping plane. The valid range for the FOV is from 1 to 179 degrees.
+  // Console games designed to be shown on screens far away from the viewer are usually between 40 - 60 degrees, while a PC game might use a higher FOV of around 90 since the screen is likely to be right in front of the player.
+  const aspect = window.innerWidth / window.innerHeight;
+  const near = 0.1; // the near clipping plane - le plan le + rapproché auquel la caméra a accès
+  const far = 100; // the far clipping plane - le plan le + éloigné qui est encore dans le champs de la caméra
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  // every object is initially created at ( 0, 0, 0 ); we'll move the camera back a bit so that we can view the scene
+  camera.position.set(0, 0, 10);
   camera.lookAt(0, 0, 0);
 }
 
-function createRenderer() {
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  container.appendChild(renderer.domElement);
+function createMaterials() {
+  //material = new THREE.MeshBasicMaterial(); // // create a default (white) Basic material - always use this in doubt, because it will still appear on the screen, even tho we forget to light up the scene
+  material = new THREE.MeshStandardMaterial({ color: 0x800080 }); // creates a Standard material that accepts light. But we have to add it to the scene!
 }
 
-function createMaterials() {
-  material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+function createLight() {
+  // by default, a room is without light. If we use Basic Mesh, it will still appear even if we don't turn on lights, but for other Meshs to work, we need to add add light to the room (the room itself can have colors, but this is the background of the scene)
+  const light = new THREE.DirectionalLight(0xffffff, 5.0);
+  // move the light back and up a bit
+  light.position.set(10, 10, 10);
+  // remember to add the light to the scene
+  scene.add(light);
 }
 
 function createGeometries() {
-  var points = [];
-  points.push(new THREE.Vector3(-10, 0, 0));
-  points.push(new THREE.Vector3(0, 10, 0));
-  points.push(new THREE.Vector3(10, 0, 0));
-
-  var geometry = new THREE.BufferGeometry().setFromPoints(points);
-  var line = new THREE.Line(geometry, material);
-  scene.add(line);
+  geometry = new THREE.BoxBufferGeometry(2, 2, 2);
 }
 
-function loadFont() {
-  var loader = new THREE.FontLoader();
-  console.log("heyhey");
-
-  loader.load(
-    "../node_modules/three/examples/fonts/helvetiker_regular.typeface.json",
-    function (responseFont) {
-      var geometry = new THREE.TextGeometry("Hello three.js!", {
-        font: responseFont,
-        size: 80,
-        height: 5,
-        curveSegments: 12,
-        bevelEnabled: true,
-        bevelThickness: 10,
-        bevelSize: 8,
-        bevelOffset: 0,
-        bevelSegments: 5,
-      });
-    }
-  );
+function createMesh() {
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh); // adding to the scene the object that we just created
 }
 
-function render() {
-  renderer.render(scene, camera);
+function createRenderer() {
+  renderer = new THREE.WebGLRenderer(); // creates the canvas in the html
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  container.appendChild(renderer.domElement);
+}
+
+function animate() {
+  requestAnimationFrame(animate); // 60 frames per second
+  renderer.render(scene, camera); // renders a still image of the scene from the point of view of the camera and outputs that picture into the <canvas> element.
 }
 
 function onWindowResize() {
@@ -91,6 +79,5 @@ function onWindowResize() {
 
 window.addEventListener("resize", onWindowResize);
 
-document.getElementById("load-font").addEventListener("click", loadFont);
-
 init();
+animate();
